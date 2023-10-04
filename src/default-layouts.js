f27d57ce03bc6252d47499d02c7637e8ca97f7be
Jesse <jessebot.git@gmail.com>
2023-10-04 16:27:54 -0700

    add temp default handlebars layouts

---
+++ src/default-layouts.js
@@ -0,0 +1,50 @@
+const INDEX = `
+<!DOCTYPE html>
+<html lang="{{ languageCode }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ title }}</title>
+  </head>
+  <body>
+    <div id="app"></div>
+  </body>
+</html>
+`;
+
+const BOOK_INDEX = `
+<!DOCTYPE html>
+<html lang="{{ languageCode }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ title }}</title>
+  </head>
+  <body>
+    <h1>Content:</h1>
+
+    {{{ content }}}
+  </body>
+</html>
+`;
+
+const CHAPTER = `
+<!DOCTYPE html>
+<html lang="{{ languageCode }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ title }}</title>
+  </head>
+  <body>
+    {{{ content }}}
+  </body>
+</html>
+`;
+
+exports.INDEX = INDEX;
+exports.BOOK_INDEX = BOOK_INDEX;
+exports.CHAPTER = CHAPTER;
