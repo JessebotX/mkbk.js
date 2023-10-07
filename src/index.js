db1070fd704284bdfe4afbce520e325e4ec1f296
Jesse <jessebot.git@gmail.com>
2023-10-07 00:39:03 -0700

    export from index.js and bump version

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const collection = require('./collection');
+const book = require('./book');
+const chapter = require('./chapter');
+const renderer = require('./renderer');
+
+exports.collection = collection;
+exports.book = book;
+exports.chapter = chapter;
+exports.renderer = renderer;
