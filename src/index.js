efb8674e0aefdbc40babbe01da789534e3c93675
Jesse <jessebot.git@gmail.com>
2023-10-17 15:56:33 -0700

    delete old js files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const collection = require('./collection');
-const book = require('./book');
-const chapter = require('./chapter');
-const renderer = require('./renderer');
-
-exports.collection = collection;
-exports.book = book;
-exports.chapter = chapter;
-exports.renderer = renderer;
