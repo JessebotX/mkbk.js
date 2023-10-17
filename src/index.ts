1cb2569bd4b339b0f0d5908482347a5309b7a161
Jesse <jessebot.git@gmail.com>
2023-10-17 16:15:54 -0700

    export everything

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import * as path from 'path';
-
-console.log(`${path.join('src', 'testdata')}`);
+export * as collection from './collection';
+export * as book from './book';
+export * as chapter from './chapter';
+export * as renderer from './renderer';
