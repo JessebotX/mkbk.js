0ca4bf511dc2b7de2045493c5c57f843805abaa0
Jesse <jessebot.git@gmail.com>
2023-11-09 12:58:10 -0800

    untabify

--- src/collection.ts
+++ src/collection.ts
@@ -4,94 +4,94 @@ import { Book, BookOptions } from './book';
 import * as book from './book'
 
 interface CollectionOptions {
-	title: string;
-	workingDir?: string;
-	languageCode?: string;
-	baseURL?: string;
-	description?: string;
-	customCSSRelPath?: string;
-	customJSRelPath?: string;
-	layoutRelDir?: string;
-	outputRelDir?: string;
-	faviconRelPath?: string;
-	books?: BookOptions[];
+    title: string;
+    workingDir?: string;
+    languageCode?: string;
+    baseURL?: string;
+    description?: string;
+    customCSSRelPath?: string;
+    customJSRelPath?: string;
+    layoutRelDir?: string;
+    outputRelDir?: string;
+    faviconRelPath?: string;
+    books?: BookOptions[];
 }
 
 export interface Collection extends CollectionOptions {
-	books?: Book[];
-	title: string;
-	languageCode: string;
-	workingDir: string;
-	layoutRelDir: string;
-	outputRelDir: string;
-	params: CollectionOptions;
+    books?: Book[];
+    title: string;
+    languageCode: string;
+    workingDir: string;
+    layoutRelDir: string;
+    outputRelDir: string;
+    params: CollectionOptions;
 }
 
 /**
  * Parse a collection of books.
  */
 export function parse(options: CollectionOptions): Collection {
-	const {
-		title,
-		baseURL,
-		description,
-		faviconRelPath,
-		customCSSRelPath,
-		customJSRelPath,
-		books
-	} = options;
+    const {
+        title,
+        baseURL,
+        description,
+        faviconRelPath,
+        customCSSRelPath,
+        customJSRelPath,
+        books
+    } = options;
 
-	let { languageCode, workingDir, layoutRelDir, outputRelDir } = options;
+    let { languageCode, workingDir, layoutRelDir, outputRelDir } = options;
 
-	if (!languageCode) {
-		languageCode = 'en';
-	}
+    if (!languageCode) {
+        languageCode = 'en';
+    }
 
-	if (!workingDir) {
-		workingDir = '.';
-	}
+    if (!workingDir) {
+        workingDir = '.';
+    }
 
-	if (!layoutRelDir) {
-		layoutRelDir = 'layout'
-	}
+    if (!layoutRelDir) {
+        layoutRelDir = 'layout'
+    }
 
-	if (!outputRelDir) {
-		outputRelDir = 'out'
-	}
+    if (!outputRelDir) {
+        outputRelDir = 'out'
+    }
 
-	const collection: Collection = {
-		title,
-		languageCode,
-		workingDir,
-		layoutRelDir,
-		outputRelDir,
-		baseURL,
-		description,
-		faviconRelPath,
-		customCSSRelPath,
-		customJSRelPath,
-		params: options,
-	};
+    const collection: Collection = {
+        title,
+        languageCode,
+        workingDir,
+        layoutRelDir,
+        outputRelDir,
+        baseURL,
+        description,
+        faviconRelPath,
+        customCSSRelPath,
+        customJSRelPath,
+        params: options,
+    };
 
-	if (!books) {
-		return collection;
-	}
+    if (!books) {
+        return collection;
+    }
 
-	collection.books = [];
-	for (let i = 0; i < books.length; i++) {
-		// check for duplicate identifiers
-		for (let j = i + 1; j < books.length; j++) {
-			if (books[i].id === books[j].id) {
-				throw new Error("More than 1 book has the same identifier");
-			}
-		}
+    collection.books = [];
+    for (let i = 0; i < books.length; i++) {
+        // check for duplicate identifiers
+        for (let j = i + 1; j < books.length; j++) {
+            if (books[i].id === books[j].id) {
+                throw new Error("More than 1 book has the same identifier");
+            }
+        }
 
-		// parse the book
-		const newBook = book.parse(books[i]);
-		collection.books.push(newBook);
-	}
+        // parse the book
+        const newBook = book.parse(books[i]);
+        collection.books.push(newBook);
+    }
 
-	return collection;
+    return collection;
 }
 
 // console.log(parse({title:'hello',
